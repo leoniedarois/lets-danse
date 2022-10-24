cd7b80caf41c4bf267ee2c8f48e8b9881eea56f1
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-10-24 19:17:31 +0200

    [prod] - try to fix prod

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import * as THREE from "../node_modules/three/build/three.module.js"
-import * as dat from 'dat.gui'
+import * as THREE from '../node_modules/three/build/three.module.js'
+import * as dat from '../node_modules/dat.gui'
 
 import Engine from '../src/engine'
 import audio from '../src/utils/audio'
