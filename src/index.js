e2647f40ddea902dc5b77b00958e03ec2a35dc5f
leoniedarois <50202798+leoniedarois@users.noreply.github.com>
2022-10-24 19:14:25 +0200

    [prod] - try to fix prod

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from "../node_modules/three/build/three.module.js"
 import * as dat from 'dat.gui'
 
 import Engine from '../src/engine'
@@ -53,7 +53,7 @@ const setupScene = () => {
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
   directionalLight.castShadow = true
   directionalLight.shadow.mapSize.set(1024, 1024)
-  directionalLight.shadow.camera.far = 15
+  directionalLight.shadow.camera.far = 500
   directionalLight.shadow.camera.left = -7
   directionalLight.shadow.camera.top = 7
   directionalLight.shadow.camera.right = 7
